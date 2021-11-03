f501e3a25354bd37cf45d28efc6894fd6ca2f231
pythonlolol <93446531+pythonlolol@users.noreply.github.com>
2021-11-03 10:06:44 -0400

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+from tiempo import *
+import os
+#este es un ejemplo del el modulo time
+os.system("cls")
+time(1,101)
+prueba = False
+time(1,100)
+time(1,100)
+if prueba == True:
+    input("verdad ")
+if prueba is not True:
+    input("mentira")
---
+++ tiempo.py
@@ -0,0 +1,7 @@
+import os
+def time(inicio,final):
+    #time creado por pythonlolol
+    # para usar este modulo tienes que poner el numero donde inicia y el numero donde finalisa
+    for i in range(inicio,final):
+        i
+        os.system("cls")
